0e2995eab8db8496b861b38237636cd059cbaf4c
Lloyd McCarthy <mccarthy@jimmy.harvard.edu>
2018-03-05 11:01:51 -0500

    added build fail on google auth missing

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ ! -f .google_auth.json ]; then
+	echo "Google authorization file not found! Please contact Lloyd for access"
+	exit 1
+fi
+
 docker build -t "celery-taskmanager" .
