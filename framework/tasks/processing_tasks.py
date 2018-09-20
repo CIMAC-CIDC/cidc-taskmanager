aba0f4c381b5e735ed57c80d3bcf06d5700760ba
Lloyd McCarthy <lloydmccar@gmail.com>
2018-09-20 13:57:35 -0400

    Added a first pass at olink validation function. Also switched over to python-black for formatter

--- framework/tasks/processing_tasks.py
+++ framework/tasks/processing_tasks.py
@@ -14,17 +14,18 @@ from celery import group
 from framework.tasks.AuthorizedTask import AuthorizedTask
 from framework.celery.celery import APP
 from framework.tasks.variables import EVE_URL
+from framework.tasks.process_npx import process_olink_npx
 
 HAPLOTYPE_FIELD_NAMES = [
-    'allele_group',
-    'hla_allele',
-    'synonymous_mutation',
-    'non_coding_mutation'
+    "allele_group",
+    "hla_allele",
+    "synonymous_mutation",
+    "non_coding_mutation",
 ]
 
 
 def process_hla_file(
-        hla_path: str, trial_id: str, assay_id: str, record_id: str
+    hla_path: str, trial_id: str, assay_id: str, record_id: str
 ) -> dict:
     """
     Turns an .hla file into an array of HLA mongo entries.
@@ -39,7 +40,7 @@ def process_hla_file(
         [dict] -- Array of HLA mongo records.
     """
     hla_records = []
-    with open(hla_path, 'r', 8192) as hla:
+    with open(hla_path, "r", 8192) as hla:
         for line in hla:
             # Split into columns
             columns = line.split()
@@ -49,46 +50,52 @@ def process_hla_file(
                     "haplotypes": [],
                     "assay": assay_id,
                     "trial": trial_id,
-                    "record_id": record_id
+                    "record_id": record_id,
                 }
                 for i in range(1, len(columns)):
                     haplotype = columns[i]
-                    haplotype_fields = haplotype.split('_')
+                    haplotype_fields = haplotype.split("_")
                     haplotype_record = {}
 
                     # Check for suffix.
                     if len(haplotype_fields) > 1:
                         # If suffix is found, store value, then trim.
                         if not str.isdigit(haplotype_fields[-1][-1]):
-                            haplotype_record['suffix'] = haplotype_fields[-1][-1]
+                            haplotype_record["suffix"] = haplotype_fields[-1][-1]
                             haplotype_fields[-1] = haplotype_fields[-1][:-1]
 
                     # Add all parts of the record that are present.
                     for j in range(2, len(haplotype_fields)):
-                        haplotype_record[HAPLOTYPE_FIELD_NAMES[j - 2]] = int(haplotype_fields[j])
+                        haplotype_record[HAPLOTYPE_FIELD_NAMES[j - 2]] = int(
+                            haplotype_fields[j]
+                        )
 
-                    hla_record['haplotypes'].append(haplotype_record)
+                    hla_record["haplotypes"].append(haplotype_record)
 
                 hla_records.append(hla_record)
             except IndexError:
-                logging.error({
-                    'message': "There was a problem with the format of the HLA file",
-                    'category': 'ERROR-CELERY-PROCESSING'
-                }, exc_info=True)
+                logging.error(
+                    {
+                        "message": "There was a problem with the format of the HLA file",
+                        "category": "ERROR-CELERY-PROCESSING",
+                    },
+                    exc_info=True,
+                )
             except ValueError:
-                logging.error({
-                    'message': "Attempted to convert a string to an int",
-                    'category': 'ERROR-CELERY-PROCESSING'
-                }, exc_info=True)
+                logging.error(
+                    {
+                        "message": "Attempted to convert a string to an int",
+                        "category": "ERROR-CELERY-PROCESSING",
+                    },
+                    exc_info=True,
+                )
 
     if hla_records:
         return hla_records
     return None
 
 
-def process_table(
-        path: str, trial_id: str, assay_id: str, record_id: str
-) -> dict:
+def process_table(path: str, trial_id: str, assay_id: str, record_id: str) -> dict:
     """
     Processes any table format data assuming the first row is a header row.
 
@@ -107,47 +114,43 @@ def process_table(
     """
     first_line = False
     entries = []
-    with open(path, 'r', 8192) as table:
+    with open(path, "r", 8192) as table:
         column_headers = []
         for line in table:
-            if line[0] != '#' and not first_line:
+            if line[0] != "#" and not first_line:
                 first_line = True
                 column_headers = [
-                    header.strip().replace('"', '').replace('.', '') for header in line.split('\t')
+                    header.strip().replace('"', "").replace(".", "")
+                    for header in line.split("\t")
                 ]
             elif first_line:
-                values = line.split('\t')
+                values = line.split("\t")
                 if not len(column_headers) == len(values):
-                    logging.error({
-                        'message': "Header and value length mismatch!",
-                        'category': 'ERROR-CELERY-PROCESSING'
-                    })
+                    logging.error(
+                        {
+                            "message": "Header and value length mismatch!",
+                            "category": "ERROR-CELERY-PROCESSING",
+                        }
+                    )
                     raise IndexError
                 entries.append(
                     dict(
-                        (
-                            column_headers[i],
-                            values[i].strip().replace('"', '')
-                        ) for i, j in enumerate(values)
+                        (column_headers[i], values[i].strip().replace('"', ""))
+                        for i, j in enumerate(values)
                     )
                 )
 
-    [entry.update(
-        {
-            'trial': trial_id,
-            'assay': assay_id,
-            'record_id': record_id
-        }
-    ) for entry in entries]
+    [
+        entry.update({"trial": trial_id, "assay": assay_id, "record_id": record_id})
+        for entry in entries
+    ]
 
     if entries:
         return entries
     return None
 
 
-def process_rsem(
-        path: str, trial_id: str, assay_id: str, record_id: str
-) -> dict:
+def process_rsem(path: str, trial_id: str, assay_id: str, record_id: str) -> dict:
     """
     Processes an rsem type table.
 
@@ -166,44 +169,46 @@ def process_rsem(
     """
     first_line = False
     entries = []
-    with open(path, 'r', 8192) as table:
+    with open(path, "r", 8192) as table:
         column_headers = []
         for line in table:
             if not first_line:
                 first_line = True
                 column_headers = [
-                    header.strip().replace('"', '').replace('.', '') for header in line.split('\t')
+                    header.strip().replace('"', "").replace(".", "")
+                    for header in line.split("\t")
                 ]
             elif first_line:
-                values = line.split('\t')
+                values = line.split("\t")
                 if not len(column_headers) == len(values):
-                    logging.error({
-                        'message': "Header and value length mismatch!",
-                        'category': 'ERROR-CELERY-PROCESSING'
-                    })
+                    logging.error(
+                        {
+                            "message": "Header and value length mismatch!",
+                            "category": "ERROR-CELERY-PROCESSING",
+                        }
+                    )
                     raise IndexError
                 entries.append(
                     dict(
                         (
                             column_headers[i],
-                            values[i].strip().replace('"', '').split(",")
-                            if column_headers[i] == 'transcript_id(s)'
-                            else values[i].strip().replace('"', '')
-                        ) for i, j in enumerate(values)
+                            values[i].strip().replace('"', "").split(",")
+                            if column_headers[i] == "transcript_id(s)"
+                            else values[i].strip().replace('"', ""),
+                        )
+                        for i, j in enumerate(values)
                     )
                 )
 
-    [entry.update(
-        {
-            'trial': trial_id,
-            'assay': assay_id,
-            'record_id': record_id
-        }
-    ) for entry in entries]
+    [
+        entry.update({"trial": trial_id, "assay": assay_id, "record_id": record_id})
+        for entry in entries
+    ]
 
     if entries:
         return entries
     return None
+
 
 # This is a dictionary of all the currently supported filetypes for storage in
 # MongoDB, with 're' indicating the regex to identify them from their
@@ -211,50 +216,21 @@ def process_rsem(
 # the key indicating the API endpoint the records are posted to.
 
 PROC = {
-    'hla': {
-        're': r'[._]hla[._]',
-        'func': process_hla_file,
+    "hla": {"re": r"[._]hla[._]", "func": process_hla_file},
+    "vcf": {"re": r".maf$", "func": process_table},
+    "neoantigen": {
+        "re": r".combined.all.binders.txt.annot.txt.clean.txt$",
+        "func": process_table,
     },
-    'vcf': {
-        're': r'.maf$',
-        'func': process_table,
-    },
-    'neoantigen': {
-        're': r'.combined.all.binders.txt.annot.txt.clean.txt$',
-        'func': process_table,
-    },
-    'purity': {
-        're': r'^Facets_output.',
-        'func': process_table,
-    },
-    'clonality_cluster': {
-        're': r'^cluster.tsv$',
-        'func': process_table,
-    },
-    'loci': {
-        're': r'^loci.tsv$',
-        'func': process_table,
-    },
-    'confints_cp': {
-        're': r'^sample_confints_CP.',
-        'func': process_table,
-    },
-    'pyclone': {
-        're': r'.pyclone.tsv$',
-        'func': process_table,
-    },
-    'cnv': {
-        're': r'_segments',
-        'func': process_table,
-    },
-    'rsem_expression': {
-        're': r'_expression.genes$',
-        'func': process_rsem,
-    },
-    'rsem_isoforms': {
-        're': r'_expression.isoforms$',
-        'func': process_table,
-    }
+    "purity": {"re": r"^Facets_output.", "func": process_table},
+    "clonality_cluster": {"re": r"^cluster.tsv$", "func": process_table},
+    "loci": {"re": r"^loci.tsv$", "func": process_table},
+    "confints_cp": {"re": r"^sample_confints_CP.", "func": process_table},
+    "pyclone": {"re": r".pyclone.tsv$", "func": process_table},
+    "cnv": {"re": r"_segments", "func": process_table},
+    "rsem_expression": {"re": r"_expression.genes$", "func": process_rsem},
+    "rsem_isoforms": {"re": r"_expression.isoforms$", "func": process_table},
+    "olink_npx": {"re": r"olink.*npx", "func": process_olink_npx},
 }
 
 
@@ -272,17 +248,14 @@ def process_file(rec: dict, pro: str) -> bool:
         boolean -- Status of the operation.
     """
     eve_fetcher = SmartFetch(EVE_URL)
-    gs_args = ['gsutil', 'cp', rec['gs_uri'], 'temp_file']
+    gs_args = ["gsutil", "cp", rec["gs_uri"], "temp_file"]
     subprocess.run(gs_args)
 
     try:
         # Use a random filename as tasks are executing in parallel.
         temp_file_name = str(uuid4())
-        records = PROC[pro]['func'](
-            'temp_file',
-            rec['trial']['$oid'],
-            rec['assay']['$oid'],
-            rec['_id']['$oid']
+        records = PROC[pro]["func"](
+            "temp_file", rec["trial"]["$oid"], rec["assay"]["$oid"], rec["_id"]["$oid"]
         )
         remove(temp_file_name)
     except OSError:
@@ -292,26 +265,31 @@ def process_file(rec: dict, pro: str) -> bool:
     try:
         eve_fetcher.post(
             endpoint=pro,
-            token=process_file.token['access_token'],
+            token=process_file.token["access_token"],
             code=201,
-            json=records
+            json=records,
         )
         for record in records:
             log = (
-                "Record: " + record['file_name'] + " added to collection: " +
-                pro + ' on trial: ' + record['trial']['$oid'] + ' on assay ' +
-                record['assay']['$oid']
+                "Record: "
+                + record["file_name"]
+                + " added to collection: "
+                + pro
+                + " on trial: "
+                + record["trial"]["$oid"]
+                + " on assay "
+                + record["assay"]["$oid"]
             )
-            logging.info({
-                'message': log,
-                'category': 'FAIR-CELERY-RECORD'
-            })
+            logging.info({"message": log, "category": "FAIR-CELERY-RECORD"})
         return True
     except RuntimeError:
-        logging.error({
-            'message': 'Biomarker upload failed',
-            'category': 'ERROR-CELERY-PROCESSING'
-        }, exc_info=True)
+        logging.error(
+            {
+                "message": "Biomarker upload failed",
+                "category": "ERROR-CELERY-PROCESSING",
+            },
+            exc_info=True,
+        )
         return False
 
 
@@ -330,22 +308,14 @@ def postprocessing(records: List[dict]) -> None:
     tasks = []
 
     for rec in records:
-        message = 'Processing: ' + rec['file_name']
-        logging.info({
-            'message': message,
-            'category': 'FAIR-CELERY-PROCESSING'
-        })
+        message = "Processing: " + rec["file_name"]
+        logging.info({"message": message, "category": "FAIR-CELERY-PROCESSING"})
         for pro in PROC:
-            if re.search(PROC[pro]['re'], rec['file_name']):
-                log = 'Match found for ' + rec['file_name']
-                logging.info({
-                    'message': log,
-                    'category': 'DEBUG-CELERY-PROCESSING'
-                })
+            if re.search(PROC[pro]["re"], rec["file_name"], re.IGNORECASE):
+                log = "Match found for " + rec["file_name"]
+                logging.info({"message": log, "category": "DEBUG-CELERY-PROCESSING"})
                 # If a match is found, add to job queue
-                tasks.append(
-                    process_file.s(rec, pro)
-                )
+                tasks.append(process_file.s(rec, pro))
 
     job = None
     # Grouping is slightly different depending on length of array.
@@ -359,10 +329,7 @@ def postprocessing(records: List[dict]) -> None:
 
     for task in tasks:
         info = "Task: " + task + " is now starting"
-        logging.info({
-            'message': info,
-            'category': 'DEBUG-CELERY-PROCESSING'
-        })
+        logging.info({"message": info, "category": "DEBUG-CELERY-PROCESSING"})
 
     # Wait for jobs to finish.
     cycles = 0
@@ -371,7 +338,10 @@ def postprocessing(records: List[dict]) -> None:
         cycles += 1
 
     if not result.successful():
-        logging.error({
-            'message': 'Error, some of the tasks failed',
-            'category': 'ERROR-CELERY-PROCESSING'
-        })
+        logging.error(
+            {
+                "message": "Error, some of the tasks failed",
+                "category": "ERROR-CELERY-PROCESSING",
+            }
+        )
+
